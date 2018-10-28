f865242a9a1491167c0664f046c99bc5d2064db8
Tobias Rahloff <rahloff@gmail.com>
2018-10-28 23:21:04 +0100

    split tests

--- tests/greetings.spec.js
+++ tests/greetings.spec.js
@@ -2,38 +2,12 @@
 /* eslint-env jest, mocha */
 
 describe('greetings', () => {
-  const greetings = require('../handlers/greetings')
-  describe('hello', () => {
-    const hello = greetings.hello
-    const context = {}
+  const greetings = require('../lib/greetingsUtil')
 
-    it('should exit immidiately if source is serverless-warmup', (done) => {
-      const event = {
-        source: 'serverless-plugin-warmup'
-      }
-
-      const callback = (ctx, data) => {
-        expect(data).toBe('Lambda is warm!')
-        done()
-      }
-
-      hello(event, context, callback)
+  it('should should return the ancient greeting "wahouli"', (done) => {
+    expect(greetings.hello()).toEqual({
+      greeting: 'wahouli'
     })
-
-    it('should should return the ancient greeting "wahouli"', (done) => {
-      const responseBody = {
-        greeting: 'wahouli'
-      }
-
-      const event = {}
-
-      const callback = (ctx, data) => {
-        expect(data.statusCode).toBe(200)
-        expect(data.body).toBe(JSON.stringify(responseBody))
-        done()
-      }
-
-      hello(event, context, callback)
-    })
+    done()
   })
 })
---
+++ tests/handlers.spec.js
@@ -0,0 +1,34 @@
+/** WORK IN PROGRESS */
+/* eslint-env jest, mocha */
+
+describe('handlers', () => {
+  describe('greetings', () => {
+    const greetings = require('../handlers/greetings')
+    const hello = greetings.hello
+    const context = {}
+
+    it('should exit immidiately if source is serverless-warmup', (done) => {
+      const event = {
+        source: 'serverless-plugin-warmup'
+      }
+
+      const callback = (ctx, data) => {
+        expect(data).toBe('Lambda is warm!')
+        done()
+      }
+
+      hello(event, context, callback)
+    })
+
+    it('build valid Lambda HTTP Response', (done) => {
+      const event = {}
+
+      hello(event, context, (ctx, data) => {
+        expect(data.statusCode).toBe(200)
+        expect(typeof data.body).toBe('string')
+        expect(typeof JSON.parse(data.body)).toBe('object')
+        done()
+      })
+    })
+  })
+})
